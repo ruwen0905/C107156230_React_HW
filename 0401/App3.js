6edab51372990111c859cda51327f3af288e02df
ruwen0905 <ruwen0905@gmail.com>
2021-06-22 04:05:38 +0800

    0401 class function arrowfunction practice

---
+++ 0401/App3.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const App = () => {
+   return (
+      <div>
+         <Header/>
+         <Content/>
+      </div>
+   );
+}
+
+const Header = () => {
+   return (
+      <div>
+         <h1>Header</h1>
+      </div>
+   );
+}
+
+const Content = () => {
+   return (
+      <div>
+         <h2>Content</h2>
+         <p>The content text!!!</p>
+      </div>
+   );
+}
+
+export default App;
